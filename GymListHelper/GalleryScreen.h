f621382382c4c6cdbb0c9f4c1611d23a78ad9a97
BrunoSkel <brunorochaesilva@gmail.com>
2015-04-15 08:18:00 -0300

    Revert 5d2bf1e..33bccd4
    This rolls back to commit 5d2bf1e3f8b1f5a9220c0941c00692f3f7101742.

--- GymListHelper/GalleryScreen.h
+++ GymListHelper/GalleryScreen.h
@@ -9,9 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface GalleryScreen : UIViewController <UITableViewDelegate, UITableViewDataSource>
-
-    @property (nonatomic) NSInteger ChosenLanguage;
-
     @property NSInteger ChosenCategory;
     @property NSString* ChosenCategoryName;
 @end
--- GymListHelper/NewChartDescriptionEditor.h
+++ GymListHelper/NewChartDescriptionEditor.h
@@ -11,19 +11,9 @@
 @interface NewChartDescriptionEditor : UIViewController <UITextFieldDelegate>
 
 @property int EditThisRoutine;
-@property BOOL isEdit;
-@property NSMutableArray* sentNameArray;
-@property NSMutableArray* sentCategorieArray;
-
-@property (strong, nonatomic) NSMutableArray *ChartCategories;
-
-@property (strong, nonatomic) IBOutlet UITextField *ChartNameNew;
-//@property (strong, nonatomic) IBOutlet UITextField *ChartCategorieNew;
-//@property (strong, nonatomic) IBOutlet UITextField *ChartDescriptionNew;
-//@property (strong, nonatomic) IBOutlet UITextField *ChartEstimatedTimeNew;
-//@property (strong, nonatomic) IBOutlet UITextField *ChartLanguageNew;
-@property (strong, nonatomic) IBOutlet UILabel *MainLabel;
-
 -(void)editMode;
-
+@property NSMutableArray* sentNameArray;
+@property BOOL isEdit;
+@property (strong, nonatomic) IBOutlet UITextField *ChartNameNew;
+@property (strong, nonatomic) IBOutlet UILabel *MainLabel;
 @end
